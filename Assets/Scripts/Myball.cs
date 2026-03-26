7bbda50b0be0fbc81d491fcbb9c4b11395b5fa12
whxogud9090 <th870707@gmail.com>
2026-03-26 12:09:04 +0900

    [Fead] 유니티 충돌 Scene 제작 , Voxel 임포트

---
+++ Assets/Scripts/Myball.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Myball : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisonEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name + " 와 충돌함");
+        
+        if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("땅과 충돌");
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("트리거 안으로 들어옴");
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("트리거 밖으로 나감");
+    }
+
+}
+
